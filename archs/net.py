f27d056d34a926de9f0d752e7985c0a84f9fe9e3
Megh <33202960+meghbhalerao@users.noreply.github.com>
2020-11-12 15:45:11 +0530

    added classification head

--- archs/net.py
+++ archs/net.py
@@ -3,7 +3,7 @@ from torchvision import models
 import torch.nn.functional as F
 
 class AlexNet(nn.Module):
-    def __init__(self,criterion, num_classes=10):
+    def __init__(self,criterion, num_classes=10, feat = False):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
@@ -20,15 +20,25 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
         )
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 2 * 2, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
+        if feat:
+            self.classifier = nn.Sequential(
+                nn.Dropout(),
+                nn.Linear(256 * 2 * 2, 4096),
+                nn.ReLU(inplace=True),
+                nn.Dropout(),
+                nn.Linear(4096, 4096),
+                nn.ReLU(inplace=True)
+            )
+        else:
+            self.classifier = nn.Sequential(
+                nn.Dropout(),
+                nn.Linear(256 * 2 * 2, 4096),
+                nn.ReLU(inplace=True),
+                nn.Dropout(),
+                nn.Linear(4096, 4096),
+                nn.ReLU(inplace=True),
+                nn.Linear(4096, num_classes),
+            )
         self._criterion = criterion()
 
     def forward(self, x):
@@ -40,3 +50,37 @@ class AlexNet(nn.Module):
     def _loss(self, input, target, mask):
         logits = self(input)
         return self._criterion(logits, target, mask) 
+    
+    
+class Predictor(nn.Module):
+    def __init__(self, num_class=10, inc=4096, temp=0.05):
+        super(Predictor, self).__init__()
+        self.fc = nn.Linear(inc, num_class, bias=False)
+        self.num_class = num_class
+        self.temp = temp
+
+    def forward(self, x, reverse=False, eta=0.1):
+        if reverse:
+            x = grad_reverse(x, eta)
+        x = F.normalize(x)
+        x_out = self.fc(x) / self.temp
+        return x_out
+    
+ 
+class Predictor_deep(nn.Module):
+    def __init__(self, num_class=10, inc=4096, temp=0.05):
+        super(Predictor_deep, self).__init__()
+        self.fc1 = nn.Linear(inc, 512)
+        self.fc2 = nn.Linear(512, num_class, bias=False)
+        self.num_class = num_class
+        self.temp = temp
+
+    def forward(self, x, reverse=False, eta=0.1):
+        x = self.fc1(x)
+        if reverse:
+            x = grad_reverse(x, eta)
+        x = F.normalize(x)
+        x_out = self.fc2(x) / self.temp
+        return x_out
+
+
