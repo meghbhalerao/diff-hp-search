correct function for hessian for ssl

archs/net.py
@@ -42,7 +42,8 @@ class AlexNet(nn.Module):
         else:
             logits = self(input)
         logits = self(input)
-        return self._criterion(logits, target, mask)    
+        return self._criterion(logits, target, mask) 
+
     
 class Predictor(nn.Module):
     def __init__(self, num_class=10, inc=4096, temp=0.05):

utils/operations.py
@@ -1,9 +1,10 @@
 import torch
+from utils.ssl_utils import *
 
 def _concat(xs):
   return torch.cat([x.view(-1) for x in xs])
 
-def hessian_vector_product(model, vector, input, target, weights,  r=1e-2):
+def hessian_vector_product(model, vector, input, target, weights, r=1e-2):
     R = r / _concat(vector).norm()
 
     for p, v in zip(model.parameters(), vector):
@@ -22,21 +23,22 @@ def hessian_vector_product(model, vector, input, target, weights,  r=1e-2):
 
     return [(x-y).div_(2*R) for x, y in zip(grads_p, grads_n)]
 
-def hessian_vector_product_2(model, vector, input, target, weights,  r=1e-2):
+def hessian_vector_product_ssl(W, W_k, img_ssl_q, img_ssl_k, vector, weights,  r=1e-2):
     R = r / _concat(vector).norm()
 
-    for p, v in zip(model.parameters(), vector):
+    for p, v in zip(W.parameters(), vector):
       p.data.add_(R, v)
 
-    loss = model._loss(input, target, weights)
+    loss = ssl_step(W,W_k, img_ssl_q, img_ssl_k)
     grads_p = torch.autograd.grad(loss,weights)
 
-    for p, v in zip(model.parameters(), vector):
+    for p, v in zip(W.parameters(), vector):
       p.data.sub_(2*R, v)
-    loss = model._loss(input, target, weights)
+    loss = ssl_step(W,W_k, img_ssl_q, img_ssl_k)
+    
     grads_n = torch.autograd.grad(loss, weights)
 
-    for p, v in zip(model.parameters(), vector):
+    for p, v in zip(W.parameters(), vector):
       p.data.add_(R, v)
 
     return [(x-y).div_(2*R) for x, y in zip(grads_p, grads_n)]

utils/ssl_utils.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-def ssl_step(W, W_k, optimizer, img_ssl_q, img_ssl_k, queue, queue_ptr, K):
+def ssl_step(W, W_k, img_ssl_q, img_ssl_k, queue = None, queue_ptr = None, K = None, A_ssl = None):
     q = W(img_ssl_q) #query
     k = W_k(img_ssl_k) #k
     k = k.detach()
@@ -10,9 +10,12 @@ def ssl_step(W, W_k, optimizer, img_ssl_q, img_ssl_k, queue, queue_ptr, K):
     logits = torch.cat([l_pos, l_neg], dim=1)
     logits /= 0.07
     labels = torch.zeros(logits.shape[0], dtype=torch.long).cuda()
+    # Here mask needs to be added
     loss_ssl = nn.CrossEntropyLoss(logits, labels)
-    optimizer.step()
-    dequeue_and_enqueue(k, queue, queue_ptr, K)
+    if queue is not None:
+        momentum_update_key_encoder(W,W_k, 0.99)
+        dequeue_and_enqueue(k, queue, queue_ptr, K)
+    return loss_ssl
 
 def momentum_update_key_encoder(encoder_q, encoder_k, m):
     for param_q, param_k in zip(encoder_q.parameters(), encoder_k.parameters()):